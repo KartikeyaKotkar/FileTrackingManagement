70aa275518403babd7d3524e35ca9362d9fa81ac
KartikeyaKotkar <kkotkar2004@gmail.com>
2026-03-11 15:23:32 +0530

    Create get_user_by_login.sql

---
+++ sql/users/get_user_by_login.sql
@@ -0,0 +1,19 @@
+-- Login: match username OR email + password
+-- Only returns active, non-deleted users
+SELECT
+    u.id,
+    u.username,
+    u.fullname,
+    u.email,
+    u.phone,
+    u.role_id,
+    r.role_name,
+    u.is_active,
+    u.created_at
+FROM app_user u
+LEFT JOIN role r ON u.role_id = r.id
+WHERE (u.username = ? OR u.email = ?)
+  AND u.password   = ?
+  AND u.is_active  = 1
+  AND u.is_deleted = 0
+LIMIT 1;
