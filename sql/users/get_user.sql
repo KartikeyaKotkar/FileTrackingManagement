eba0f46954924ba620e84c35a137afcd54b0a7dc
KartikeyaKotkar <kkotkar2004@gmail.com>
2026-03-11 15:23:30 +0530

    Create get_user.sql

---
+++ sql/users/get_user.sql
@@ -0,0 +1,15 @@
+SELECT
+    u.id,
+    u.username,
+    u.fullname,
+    u.email,
+    u.phone,
+    u.role_id,
+    r.role_name,
+    u.is_active,
+    u.created_at
+FROM app_user u
+LEFT JOIN role r ON u.role_id = r.id
+WHERE u.id         = ?
+  AND u.is_deleted = 0
+LIMIT 1;
