d7f9b474fa4a19b09b86ab09db98811cfb2e816f
Kartikeya Kotkar <kkotkar2004@gmail.com>
2026-06-01 16:14:29 +0530

    Update 02_seed_data.sql

--- SQL/schema/02_seed_data.sql
+++ SQL/schema/02_seed_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (id, name) VALUES (1, 'Admin'), (2, 'IT'), (3, 'Finance'), (4, 'HR') ON CONFLICT (id) DO NOTHING;
+INSERT INTO department (id, name) VALUES (1, 'Admin') ON CONFLICT (id) DO NOTHING;
 INSERT INTO role (id, role_name) VALUES (1, 'Admin'), (2, 'Manager'), (3, 'User') ON CONFLICT (id) DO NOTHING;
 INSERT INTO app_user (id, username, fullname, password, role_id, department_id, is_active, is_deleted)
 VALUES (
